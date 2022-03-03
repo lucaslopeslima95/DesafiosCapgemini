17da318bb2b6196ba9514aa01289ab2bb43a5ab6
lucaslopeslima95 <lucaslopesllima@gmail.com>
2022-03-02 21:57:20 -0300

    subindo terceiro desafio

--- DesafiosCapgemini/src/main/java/Desafios/Desafio03.java
+++ DesafiosCapgemini/src/main/java/Desafios/Desafio03.java
@@ -1,9 +1,37 @@
 package Desafios;
 
+
 public class Desafio03 {
 
 	
 	public static void main(String[] args) {
+		retornaStringCriptografada("tenha um bom dia");
+	}
+	
+	public static String retornaStringCriptografada(String string) {
+		String semEspaço = string.replace(" ", "");
+		char[] arrayStringParaCriptografar = semEspaço.toCharArray();
+		int posicaoMax = semEspaço.length()-1;
+		int indice = 0;
+		int dimencao = (int) Math.ceil(Math.sqrt(semEspaço.length()));
+		char[][] criptografador = new char[dimencao][dimencao];
+		
+		for (int i = 0; i < criptografador.length; i++) {
+			for (int j = 0; j < criptografador.length; j++) {
+				
+				criptografador[j][i] = arrayStringParaCriptografar[indice];
+				if(posicaoMax > indice) {
+					indice++;
+				}
+			}
+		}
+		for (int i = 0; i < criptografador.length; i++) {
+			for (int j = 0; j < criptografador.length; j++) {
+				System.out.print(criptografador[i][j]);
+			}
+			System.out.print(" ");
+		}
+		return null;
 		
 	}
 }
