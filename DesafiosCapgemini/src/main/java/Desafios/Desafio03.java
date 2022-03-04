6dbf9e15e1cb4019e7762092a9815757ec803ba3
Lucas Lopes Lima <lucaslopesllima@gmail.com>
2022-03-04 15:14:29 -0300

    Update 2 Chalenge 03
    Foi adicionado a filtragem de qual raiz ultilizar em casa de numero querbrado ou raiz exatas.

--- DesafiosCapgemini/src/main/java/Desafios/Desafio03.java
+++ DesafiosCapgemini/src/main/java/Desafios/Desafio03.java
@@ -1,33 +1,50 @@
 package Desafios;
 
-import java.util.Scanner;
 
 public class Desafio03 {
 
 	public static void main(String[] args) {
-		
-		System.out.println(retornaStringCriptografada("tenha um bom dia "));
+		System.out.println(retornaStringCriptografada("Ola Mundo"));
 	}
 
 	public static String retornaStringCriptografada(String textoParaCriptografar) {
+		int indiceParaPercorreOArrayComAsLetrasDoTexto = 0;
+		int dimencaoDaMatriz = 0;
+		int valorMaiorDaRaizDoTamanhoTexto = 0;
+		int valorMenorDaRaizDoTamanhoTexto = 0;
 
-		String textoSemEspaÁos = textoParaCriptografar.replace(" ", "");
+		//.replace, substitui o 1¬∞ parametro pelo segundo
+		String textoSemEspa√ßos = textoParaCriptografar.replace(" ", "");
 
 		String textoCriptografado = "";
 
-		char[] arrayDoTextoParaCriptografar = textoSemEspaÁos.toCharArray();
+		char[] arrayDoTextoParaCriptografar = textoSemEspa√ßos.toCharArray();
 
-		int indiceParaPercorreOArrayComAsLetrasDoTexto = 0;
 		// Math.sqrt - Retorna a raiz quadrada, Math.Ceil Retorna o numero arredondado
-		// para cima
-		int dimencao = (int) Math.ceil(Math.sqrt(textoSemEspaÁos.length()));
+		// para cima; Math . pow retorna potencia do primeiro pelo segundo argumento
+		valorMaiorDaRaizDoTamanhoTexto = (int) Math.ceil(Math.sqrt(textoSemEspa√ßos.length()));
+		
+		valorMenorDaRaizDoTamanhoTexto = (int) Math.floor(Math.sqrt(textoSemEspa√ßos.length()));
 
-		char[][] matrizParaCriptografar = new char[dimencao][dimencao];
+		if (valorMaiorDaRaizDoTamanhoTexto == valorMenorDaRaizDoTamanhoTexto) {
+			dimencaoDaMatriz = valorMaiorDaRaizDoTamanhoTexto;
+			
+		} else if ((valorMenorDaRaizDoTamanhoTexto * valorMenorDaRaizDoTamanhoTexto) >= textoSemEspa√ßos.length()) {
+
+			dimencaoDaMatriz = valorMenorDaRaizDoTamanhoTexto;
+			
+		} else {
+			
+			dimencaoDaMatriz = valorMaiorDaRaizDoTamanhoTexto;
+
+		}
+
+		char[][] matrizParaCriptografar = new char[dimencaoDaMatriz][dimencaoDaMatriz];
 
 		for (int i = 0; i < matrizParaCriptografar.length; i++) {
 			for (int j = 0; j < matrizParaCriptografar.length; j++) {
 
-				if (indiceParaPercorreOArrayComAsLetrasDoTexto <= (textoSemEspaÁos.length() - 1)) {
+				if (indiceParaPercorreOArrayComAsLetrasDoTexto <= (textoSemEspa√ßos.length() - 1)) {
 
 					matrizParaCriptografar[j][i] = arrayDoTextoParaCriptografar[indiceParaPercorreOArrayComAsLetrasDoTexto];
 
@@ -39,7 +56,7 @@ public class Desafio03 {
 		for (int i = 0; i < matrizParaCriptografar.length; i++) {
 			for (int j = 0; j < matrizParaCriptografar.length; j++) {
 
-				// if pula a execuÁ„o caso a posiÁ„o na matriz seja vazia
+				// if pula a execu√ß√£o caso a posi√ß√£o na matriz seja vazia
 				if (matrizParaCriptografar[i][j] == Character.MIN_VALUE) {
 					continue;
 				}
@@ -47,7 +64,7 @@ public class Desafio03 {
 				textoCriptografado = textoCriptografado + matrizParaCriptografar[i][j];
 
 			}
-			// if ultilizado para evitar que no ultimo loop seja adicionado um espaÁo em
+			// if ultilizado para evitar que no ultimo loop seja adicionado um espa√ßo em
 			// branco
 			if (i < (matrizParaCriptografar.length - 1)) {
 				textoCriptografado = textoCriptografado + " ";
@@ -58,3 +75,4 @@ public class Desafio03 {
 
 	}
 }
+
